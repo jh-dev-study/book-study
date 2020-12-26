a94ecd352a43570fe126a6662984e874b91b1f20
JuHyun419 <zzang9ha@naver.com>
2020-12-26 18:08:32 +0900

    [chapter/3] refactoring - update annotation location

--- src/main/java/com/juhyun/book/springboot/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/juhyun/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -1,23 +1,19 @@
 package com.juhyun.book.springboot.web.dto;
 
 import com.juhyun.book.springboot.domain.posts.Posts;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
 public class PostsSaveRequestDto {
     private String title;
     private String content;
     private String author;
-
-    @Builder
-    public PostsSaveRequestDto(String title, String content, String author) {
-        this.title = title;
-        this.content = content;
-        this.author = author;
-    }
 
     public Posts toEntity() {
         return Posts.builder()
