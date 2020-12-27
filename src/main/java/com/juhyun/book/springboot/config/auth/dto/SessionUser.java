4442c9fc53c93e095a3a1ea8fdcb2bccc1d37c63
JuHyun419 <zzang9ha@naver.com>
2020-12-27 17:05:40 +0900

    [chapter/5] 스프링 시큐리티, OAuth 2.0으로 Google 로그인 기능 구현

---
+++ src/main/java/com/juhyun/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package com.juhyun.book.springboot.config.auth.dto;
+
+import com.juhyun.book.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
--- src/main/java/com/juhyun/book/springboot/web/IndexController.java
+++ src/main/java/com/juhyun/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.juhyun.book.springboot.web;
 
+import com.juhyun.book.springboot.config.auth.dto.SessionUser;
+import com.juhyun.book.springboot.domain.user.User;
 import com.juhyun.book.springboot.service.posts.PostsService;
 import com.juhyun.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +10,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     // 머스테치는 컨트롤러에서 문자열을 반환할 때 앞의경로와 뒤의 파일 확장자는 자동 지정됨
     // 앞의 경로 : src/main/resources/templates
@@ -21,6 +27,12 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
