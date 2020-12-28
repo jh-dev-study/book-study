da56578c1cb0bc1b2c4328f43a40d38d15279fd0
JuHyun419 <zzang9ha@naver.com>
2020-12-28 20:58:14 +0900

    [chapter/5] 네아로(네이버 아이디로 로그인) 기능 추가

--- src/main/java/com/juhyun/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/juhyun/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -27,7 +27,23 @@ public class OAuthAttributes {
     public static OAuthAttributes of(final String registrationId,
                                      final String userNameAttributeName,
                                      final Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(final String userNameAttributeName,
+                                           final Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profileImage"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(final String userNameAttributeName,
